b011470f5a2bfe7e74da3249aa27b4cfbb527e5f
Brady Baker <bradybaker82@gmail.com>
2020-11-06 21:00:38 -0600

    Completed input for employeeObject and push to array

--- client.js
+++ client.js
@@ -2,6 +2,32 @@
 
 $(readyNow);
 
-function readyNow() {
+let employeeArray = [];
 
+function readyNow() {
+    $('#btn-submit').on('click', employeeInput)
+}
+
+function employeeInput(event) {
+    event.preventDefault();
+    let employeeSalary = {
+        fName: $('#in-firstName').val(),
+        lName: $('#in-lastName').val(),
+        id: $('#in-id').val(),
+        title: $('#in-title').val(),
+        annualSalary: $('#in-annualSalary').val()
+    }
+    addEmployee(employeeSalary.fName, employeeSalary.lName, employeeSalary.id, employeeSalary.title, employeeSalary.annualSalary)
+}
+
+function addEmployee(fNameInput, lNameInput, idInput, titleInput, annualSalaryinput) {
+    let employeeObject = {
+        fName: fNameInput,
+        lName: lNameInput,
+        id: idInput,
+        title: titleInput,
+        annualSalary: annualSalaryinput,
+    }
+    employeeArray.push(employeeObject);
+    console.log('Employee Object:', employeeObject);
 }
